e7ffebc47da91cd88eccae7d71f8ba464aff9894
Jeremy Samuel <pentium6.0@gmail.com>
2020-08-23 02:15:08 -0400

    fixing multiple load bug

--- src/store/modules/dataManagement.js
+++ src/store/modules/dataManagement.js
@@ -16,7 +16,7 @@ const mutations = {
       .then(res => {
         const data = res.data
         for (const key in data) {
-          state.myActivities.push(...data[key].myActivities)
+          myActivities.state.activities.push(...data[key].myActivities)
         }
       })
       .catch(err => console.log(err))
@@ -32,15 +32,13 @@ const actions = {
   saveData: ({ commit }) => {
     commit('CLEAR_DATA')
     commit('SAVE_DATA', myActivities.state.activities)
-    state.snackbar = true
-    state.snackbarText = 'Activities Saved!'
   },
   loadData: ({ commit }) => {
     commit('LOAD_DATA')
   },
   clearData: ({ commit }) => {
-    commit('CLEAR_DATA')
     myActivities.state.activities = []
+    commit('CLEAR_DATA')
   }
 }
 
