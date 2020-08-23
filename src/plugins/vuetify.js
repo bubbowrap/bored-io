10c77f96ba99f1e21f151f003ecaf0fcaece4c72
Jeremy Samuel <pentium6.0@gmail.com>
2020-08-23 14:34:27 -0400

    update dark card color

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -13,16 +13,16 @@ export default new Vuetify({
         headingColor: colors.indigo,
         bodyColor: colors.grey.darken2,
         notifBar: colors.grey.lighten2,
+        cardBackground: '#FFFFFF',
         primary: colors.indigo,
-        secondary: colors.orange.darken2,
-        accent: colors.shades.black,
-        error: colors.red.accent3
+        secondary: colors.orange.darken2
       },
       dark: {
         background: '#121212',
         headingColor: colors.grey.lighten5,
         bodyColor: colors.grey.lighten5,
         notifBar: colors.grey.darken4,
+        cardBackground: '#272727',
         primary: colors.indigo.darken2,
         secondary: colors.orange.darken2
       }
